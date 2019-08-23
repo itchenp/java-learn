01d82de578e090c4ce5e0615182211f8023f3dfb
itchenp <itchenp@gmail.com>
2019-08-23 23:10:45 +0800

    init

---
+++ java-source-learn/src/main/java/com/ithub/source/learn/SourceLearnApplication.java
@@ -0,0 +1,13 @@
+package com.ithub.source.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceLearnApplication.class, args);
+    }
+
+}
