f3ef2093032dff22f0bc6c90cf234a65e0bcb016
chenpeng <2wsx3edc>
2019-11-22 15:27:46 +0800

    添加spring 源码学习模块

---
+++ spring-source-learn/src/main/java/com/ithub/source/learn/SpringSourceLearnApplication.java
@@ -0,0 +1,20 @@
+package com.ithub.source.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring 源码学习
+ *
+ * @author CP.
+ * @create 2019/11/22
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class SpringSourceLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSourceLearnApplication.class, args);
+    }
+
+}
--- tool/src/main/java/com/ithub/source/learn/maven/maven-command.md
+++ tool/src/main/java/com/ithub/source/learn/maven/maven-command.md
@@ -8,3 +8,10 @@
 mvn clean -Dmaven.test.skip=true
 ###### 指定编译环境
 mvn clean -P local [^_^]: # (注意. springboot 项目不需要这里指定)
+###### 指定模块编译
+mvn dependency:analyze -pl "service/[模块名]" -Plocal
+###### 编译排除指定模块
+mvn -pl "!service/[模块名]" install -Dmaven.test.skip=true -Plocal
+###### 上传包到本地服务器
+mvn install:install-file -Dfile=[本地包路径] -DgroupId=com.jfpal -DartifactId=agent-base -Dversion=1.0.0 -Dpackaging=jar
+
