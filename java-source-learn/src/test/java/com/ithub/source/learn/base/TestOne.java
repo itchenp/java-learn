10b0563fc713bccf160b759ee266b1f8c28c158b
itchenp <itchenp@gmail.com>
2019-12-14 17:12:12 +0800

    整理添加知识点

---
+++ java-source-learn/src/test/java/com/ithub/source/learn/base/TestOne.java
@@ -0,0 +1,15 @@
+package com.ithub.source.learn.base;
+
+import org.junit.Test;
+
+/**
+ * 测试1
+ */
+public class TestOne {
+    @Test
+    public void test01(){
+        String str = " ";
+        System.out.println(str.length());
+        System.out.println(str.isEmpty());
+    }
+}
---
+++ tool/src/main/java/com/ithub/source/learn/point/package-info.java
@@ -0,0 +1,4 @@
+package com.ithub.source.learn.point;
+/**
+ * java 学习知识点及收集的面试题
+ */
