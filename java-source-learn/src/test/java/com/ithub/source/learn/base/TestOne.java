a2467f2ad6e381127b42de9770cfa0d5382faf5f
itchenp <itchenp@gmail.com>
2020-01-18 14:03:47 +0800

    hash test

--- java-source-learn/src/test/java/com/ithub/source/learn/base/TestOne.java
+++ java-source-learn/src/test/java/com/ithub/source/learn/base/TestOne.java
@@ -12,4 +12,26 @@ public class TestOne {
         System.out.println(str.length());
         System.out.println(str.isEmpty());
     }
+
+    @Test
+    public void test02(){
+        String value = "Aesfe2";
+        System.out.println(value.charAt(3));
+    }
+
+    @Test
+    public void test03(){
+        System.out.println(2 << 12);
+        System.out.println(2 >> 4);
+    }
+
+    @Test
+    public void test04(){
+        int i = 1;
+        do{
+            i++;
+        }while (i<10);
+
+        System.out.println(i);
+    }
 }
